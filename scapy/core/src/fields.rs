d3108e9405377432295336b31cba34d5d0cc5680
gpotter2 <10530980+gpotter2@users.noreply.github.com>
2023-02-25 17:16:47 +0100

    Begin Field class

--- scapy/core/src/fields.rs
+++ scapy/core/src/fields.rs
@@ -1,12 +1,15 @@
 use pyo3::prelude::*;
 
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyclass]
+struct Field {
+    name: str,
+    fmt: str,
+    default: u64,
+    sz: u8,
 }
 
 #[pymodule]
 pub fn fields(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<Field>()?;
     Ok(())
 }
